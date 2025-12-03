0be7c9187c6d93fe595c289d1c2ec36bf361a5c4
Mohammad Khavari <mohammad.khavari@powercoders.org>
2025-12-03 17:08:44 +0100

    PW button add, new Window for kiosk info added

--- WinAutoLogon_WPF/MainWindow.xaml.cs
+++ WinAutoLogon_WPF/MainWindow.xaml.cs
@@ -16,14 +16,56 @@ namespace WinAutoLogon_WPF
   /// </summary>
   public partial class MainWindow : Window
   {
+    
     public MainWindow()
     {
       InitializeComponent();
+      
     }
 
     private void BtnShowPW_Clicked(object sender, RoutedEventArgs e)
     {
       
+    }
+
+    private void PWVisibleClicked(object sender, RoutedEventArgs e)
+    {
+      Button button = (Button)sender;
+      bool isPWHiden = (txtPassword.Visibility == Visibility.Visible);
+
+      if (isPWHiden)
+      {
+        
+        TxtVisiblePWInput.Text = txtPassword.Password;
+        txtPassword.Visibility = Visibility.Collapsed;
+        button.Foreground = new SolidColorBrush(Color.FromRgb(52, 152, 219));
+        TxtVisiblePWInput.Visibility = Visibility.Visible;
+        button.Content = "👁️";
+        button.Padding = new Thickness(0);
+        button.FontSize = 12;
+        //MessageTextBlock.Text = "";
+
+      }
+      else
+      {
+        txtPassword.Password = TxtVisiblePWInput.Text;
+        TxtVisiblePWInput.Visibility = Visibility.Collapsed;
+        txtPassword.Visibility = Visibility.Visible;
+        button.Content = "‿";
+        button.Padding = new Thickness(0,-16,0,0);
+        button.FontSize = 20;
+      }
+    }
+
+    private void DetectKiosk(object sender, RoutedEventArgs e)
+    {
+      var kioskWin = new FrmKioskDetector();
+      kioskWin.ShowDialog();
+    }
+
+    private void CloseApp(object sender, RoutedEventArgs e)
+    {
+      this.Close();
     }
   }
 }
